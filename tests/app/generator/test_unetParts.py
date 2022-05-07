730270f1e1704394c40f4086c0b5837971fcb15a
Nikhil Mudhalwadkar <nmud19@gmail.com>
2022-05-08 01:03:50 +0200

    add

--- tests/app/generator/test_unetParts.py
+++ tests/app/generator/test_unetParts.py
@@ -1,10 +1,11 @@
-import pytest
 from app.generator import unetParts
 
 def test_layer1_encoder_should_return_shape()->None:
     """Checkout layer 1"""
-    layer = unetParts.encoderLayer1()
-    layer(
+    layer = unetParts.encoderLayer1( 
         in_channels=1, 
         out_channels=64
     )
+    print(layer.shape)
+
+test_layer1_encoder_should_return_shape()
