312e1ae91b22638359c54da2db9a4347eda6e032
Nikhil Mudhalwadkar <nmud19@gmail.com>
2022-05-22 16:13:05 +0200

    Created UNET architecture, fixed all tests, encoder returns conv layer and the encoded layer: TODO- fix finaly output

---
+++ tests/app/generator/test_unetGen.py
@@ -0,0 +1,25 @@
+from app.generator import unetGen
+import pytest
+import torch
+
+
+@pytest.mark.parametrize(
+    "input_size, expected_output_size",
+    [
+        (
+                torch.randn(1, 3, 572, 572),
+                torch.Size([1, 3, 388, 388])
+        ),
+    ]
+)
+def test_unet_returns_appropriate_image(
+        input_size: torch.Tensor,
+        expected_output_size: torch.Size,
+) -> None:
+    """
+    UNET returns appropriate shape
+    :return:
+    """
+    unet_obj = unetGen.UNET()
+    res = unet_obj(input_size)
+    assert res.size() == expected_output_size
