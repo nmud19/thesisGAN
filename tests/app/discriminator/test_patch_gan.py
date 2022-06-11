6aaab4d0583b17e160791fe901095725f6d11e26
Nikhil Mudhalwadkar <nmud19@gmail.com>
2022-06-11 22:37:34 +0200

    Patch GAN output

--- tests/app/discriminator/test_patch_gan.py
+++ tests/app/discriminator/test_patch_gan.py
@@ -13,13 +13,6 @@ import pytest
                 torch.randn(1, 5, 256, 256),
                 torch.randn(1, 5, 256, 256),
                 torch.Size([1, 1, 16, 16]),
-        ),
-        (
-                6,
-                1,
-                torch.randn(1, 3, 572, 572),
-                torch.randn(1, 3, 572, 572),
-                torch.Size([1, 1, 35, 35]),
         )
     ]
 )
@@ -34,25 +27,5 @@ def test_patch_gan_returns_proper_shape(
         input_channels=in_channels,
         hidden_channels=hidden_channels
     )
-    actual = obj(x, y)
+    actual = obj(x,y)
     assert actual.size() == expected_size
-
-
-@pytest.mark.parametrize(
-    "generated, real, expected_size",
-    [
-        (
-                torch.randn(1, 3, 256, 256),
-                torch.randn(1, 3, 256, 256),
-                torch.Size([1, 1, 26, 26]),
-        )
-    ]
-)
-def test_discriminator(
-        generated: torch.Tensor,
-        real: torch.Tensor,
-        expected_size:torch.Size,
-) -> None:
-    model = patch_gan.Discriminator()
-    preds = model(generated, real)
-    assert preds.size() == expected_size
